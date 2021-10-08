e8f9192feda1458d1ea44b62d2485e911f16acef
borko81 <bstoilov81@gmail.com>
2021-10-08 17:01:13 +0300

    Try to implement marshmellow, that is the end of project

---
+++ project/app.py
@@ -0,0 +1,9 @@
+from flask import Blueprint
+from flask_restful import Api
+from project.resources.hello import Hello
+
+
+api_bp = Blueprint('api', __name__)
+api = Api(api_bp)
+
+api.add_resource(Hello, '/Hello')
